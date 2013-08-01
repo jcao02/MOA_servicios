42fe1a841544e9aa15bf17432007c3a6df8843db
Juan Carlos <arochajuan2@gmail.com>
2013-07-31 20:38:44 -0500

    Mostrando ultimo documento

--- app_documento/db/schema.rb
+++ app_documento/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130731045841) do
+ActiveRecord::Schema.define(:version => 20130801012137) do
 
   create_table "dependencia", :force => true do |t|
     t.integer  "tipo_documento_id"
